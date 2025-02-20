d1df6179b0bd57f98c77b1b87d8692ea3e27b3ae
Gary Alvarado <166110913+Denilson15@users.noreply.github.com>
2025-02-19 16:09:28 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  build: {
+    outDir: "dist", // Ensure Vite outputs files to "dist"
+  },
+});
